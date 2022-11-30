361bf1b5948cdc9b9921b30240c5d25174e27d4b
HARVEYNASH\nhanphamv <nhan.phamvan@nashtechglobal.com>
2022-11-30 18:49:45 +0700

    build: handle users list table and pagination

---
+++ components/loading/Spinner.tsx
@@ -0,0 +1,13 @@
+/*
+  Spinner is a loading action to display on page, use styled-component to Css styles
+  Follow: https://github.com/andrej-naumovski/react-suspense-typescript-example
+  Styled: https://styled-components.com/docs/api#typescript
+*/
+
+// import styles from './Spinner.module.scss'
+
+const Spinner = () => {
+  return <div className="spinner"></div>
+}
+
+export default Spinner
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,8 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { userRepository } from '../../../libs/user-repository'
+import type { UserResponseTypes, IUser } from '../../../models'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<UserResponseTypes>
 ) {
+  const users = userRepository.getAll()
+  const method = req.method?.toLowerCase()
+
+  switch (method) {
+    case 'get':
+      res.status(200).json({ users })
+      break
+    default:
+      res.status(405).json({
+        message: 'Method Not Allowed'
+      })
+  }
+
   return res.status(200)
 }
