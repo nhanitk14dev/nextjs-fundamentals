bb47a9142fbe589ab63607a43f38d368bab61dba
NhanPV <nhandavid96@gmail.com>
2022-12-12 17:00:04 +0700

    build: add image optimization to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image'
+import mountainImg from '../public/mountains.jpg'
+
 export default function Home() {
-  return <>Welcome to Homepage</>
+  return (
+    <>
+      <h1>Welcome to Homepage</h1>
+      <Image
+        src={mountainImg}
+        alt="Mountain"
+        width={500}
+        height={500}
+      />
+    </>
+  )
 }
